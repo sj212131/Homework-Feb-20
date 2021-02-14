0a9c5fd08af53f8651595999e623a26d7a5962c0
shangxian Piao <sj212131@hotmail.com>
2021-02-14 18:08:36 -0500

    length fixed, display fixed, alert added

--- script.js
+++ script.js
@@ -5,22 +5,23 @@ var userselection = []
 // Write password to the #password input
 function writePassword() {
 
+  // Chart
   var lowercase_chart = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
   var Uppercase_chart = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
   var Number_chart = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-  var Symbel_chart = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+", "<", ">", "?" ]
+  var Symbel_chart = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "?", "=", "/", "|", "<", ">" ]
 
   
   // Userselections
-    var passwordrange = window.prompt('Please select the range of your password (1 ~ 128)');
-    if (1 < passwordrange && passwordrange < 128) {
-      
+    // Range
+    var passwordrange = parseInt(window.prompt('Please select the range of your password (1 ~ 128)'));
+    if (1 <= passwordrange && passwordrange <= 128) {
       console.log('password lenth confirmed');
     } else {
       window.alert ('invaild input please re-enter your range');
       return;
     }
-
+    
     // lowercase
     var includelowercase = window.confirm('Would you like to have lowercase in your password ?');
     if (includelowercase) {
@@ -40,7 +41,6 @@ function writePassword() {
 
     // Number
     var includeNumber = window.confirm ('Would you like to have number in your password ?');
-
     if (includeNumber) {
       userselection.push(...Number_chart);
       console.log(userselection);
@@ -49,26 +49,23 @@ function writePassword() {
 
     // Symbel
     var includeSymbel = window.confirm ('Would you like to have symbel in your password ?');
-
     if (includeSymbel) {
       userselection.push(...Symbel_chart);
       console.log(userselection);
       console.log('Symbel added');
     } 
-
-    console.log(userselection);
     
+    //Generate Password
     var password = [] ;
-
-    for (var i = 0, n = userselection.lenth; i < passwordrange; i++ ){
-      password += userselection[Math.floor(Math.random() * n)];
+    for (var i = 0; i < passwordrange; i++ ){
+      password.push( userselection[Math.floor(Math.random() * userselection.length)]);
     }
+    console.log(password.join(''))
 
-    console.log(password)
-
-    // var password = generatePassword();
+    //Display
     var passwordText = document.querySelector("#password");
-    passwordText.value = password;
+    passwordText.value = password.join('');
+    window.alert('Here is your Password ' + "\n" + password.join(''));
   
 return;
 }
